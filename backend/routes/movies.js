da5c973fc303664f338c2e04c5ecb540169fe31d
Mehak Sharma <119045885+mehak-ai@users.noreply.github.com>
2025-12-23 14:36:06 +0530

    Remove movie routes from movies.js

--- backend/routes/movies.js
+++ backend/routes/movies.js
@@ -2,13 +2,10 @@ const express = require('express');
 const router = express.Router();
 const movieController = require('../controllers/movieController');
 
-// GET all movies
 router.get('/', movieController.getAllMovies);
 
-// GET single movie
 router.get('/:id', movieController.getMovieById);
 
-// GET showtimes for a movie
 router.get('/:id/showtimes', movieController.getMovieShowtimes);
 
 module.exports = router;
