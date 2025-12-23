8a91f00305d68c5772d0af1ab3db040dcce1ad63
Mehak Sharma <119045885+mehak-ai@users.noreply.github.com>
2025-12-23 14:35:45 +0530

    Remove comments from bookings route file

--- backend/routes/bookings.js
+++ backend/routes/bookings.js
@@ -3,13 +3,10 @@ const router = express.Router();
 const bookingController = require('../controllers/bookingController');
 const { validateBooking } = require('../middleware/validateRequest');
 
-// POST create booking
 router.post('/', validateBooking, bookingController.createBooking);
 
-// GET booking by reference
 router.get('/:reference', bookingController.getBookingByReference);
 
-// DELETE cancel booking
 router.delete('/:reference', bookingController.cancelBooking);
 
 module.exports = router;
